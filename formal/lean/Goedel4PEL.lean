98c28827bbef650004cca4374f30a4622a2dadd1
cr4bbz <voigtjulianleon@gmail.com>
2026-08-17 21:00:57 +0200

    formal: import Fitting type split

--- formal/lean/Goedel4PEL.lean
+++ formal/lean/Goedel4PEL.lean
@@ -7,6 +7,7 @@ import Goedel4PEL.GoedelScott.GodlikeVariants
 import Goedel4PEL.GoedelScott.AndersonInterfaces
 import Goedel4PEL.GoedelScott.AndersonBilateral
 import Goedel4PEL.GoedelScott.AndersonFrames
+import Goedel4PEL.GoedelScott.FittingTypes
 import Goedel4PEL.GoedelScott.T1
 import Goedel4PEL.GoedelScott.T2
 import Goedel4PEL.GoedelScott.T3Collapse
