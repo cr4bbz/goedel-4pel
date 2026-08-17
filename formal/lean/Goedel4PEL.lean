4b326c985277a6fc22ac2418034adfdc18589fd0
cr4bbz <voigtjulianleon@gmail.com>
2026-08-17 21:35:37 +0200

    formal: import Fitting de-dicto stability bridge

--- formal/lean/Goedel4PEL.lean
+++ formal/lean/Goedel4PEL.lean
@@ -11,6 +11,7 @@ import Goedel4PEL.GoedelScott.FittingTypes
 import Goedel4PEL.GoedelScott.FittingSemantics
 import Goedel4PEL.GoedelScott.FittingNecessaryExistence
 import Goedel4PEL.GoedelScott.FittingRecovery
+import Goedel4PEL.GoedelScott.FittingDeDicto
 import Goedel4PEL.GoedelScott.T1
 import Goedel4PEL.GoedelScott.T2
 import Goedel4PEL.GoedelScott.T3Collapse
