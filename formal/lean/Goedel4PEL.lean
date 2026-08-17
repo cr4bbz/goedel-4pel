3c6be5baab60e2c886cd053946604e234656f9f1
cr4bbz <voigtjulianleon@gmail.com>
2026-08-17 19:51:13 +0200

    formal: import T1 and T3 collapse theorems

--- formal/lean/Goedel4PEL.lean
+++ formal/lean/Goedel4PEL.lean
@@ -2,5 +2,7 @@ import Goedel4PEL.FourValued.Truth
 import Goedel4PEL.FourValued.Modal
 import Goedel4PEL.FourValued.Actualist
 import Goedel4PEL.GoedelScott.Semantics
+import Goedel4PEL.GoedelScott.T1
 import Goedel4PEL.GoedelScott.T2
+import Goedel4PEL.GoedelScott.T3Collapse
 import Goedel4PEL.Analysis.Collapse
